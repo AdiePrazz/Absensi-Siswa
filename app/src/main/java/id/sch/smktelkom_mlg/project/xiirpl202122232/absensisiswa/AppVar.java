0b3d2f4ea8702358d24577cde99b774b9a1fb52b
Adi Prasetyo <adiprazz31@yahoo.com>
2016-11-13 15:03:59 +0700

    Login gurung dadi

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl202122232/absensisiswa/AppVar.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl202122232/absensisiswa/AppVar.java
@@ -4,7 +4,7 @@ package id.sch.smktelkom_mlg.project.xiirpl202122232.absensisiswa;
  * Created by A on 08/11/2016.
  */
 public class AppVar {
-    public static final String LOGIN_URL = "http://172.26.2.225/absen/login.php";
+    public static final String LOGIN_URL = "http://172.26.1.254/absen/login.php";
 
     //Keys for email and password as defined in our $_POST['key'] in login.php
     public static final String KEY_USERNAME = "username";
