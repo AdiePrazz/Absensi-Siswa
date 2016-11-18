8fcd87d7d56ddc7d3d102b8c92cae305f428c054
Resta <resta190498@gmail.com>
2016-11-18 21:16:48 +0700

    Resta Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl202122232/absensisiswa/ProsesActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl202122232.absensisiswa;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ProsesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proses);
+    }
+}
